15671e187e1d1862a3122bec4a0e95bbed5b44f0
ToastedWaffless <68156522+toastedwaffless@users.noreply.github.com>
2020-07-19 21:26:19 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+This is a zipfile brute-force password cracker that uses python3.8
+
+THIS IS ONLY FOR 
+
+INSTALL:
+
+First go to www.python.org and download the latest version of python
+
+once complete run this command in the command-line:
+
+WINDOWS:
+
+$ pip install -r REQUIREMENTS.txt
+
+LINUX/MAC:
+
+$ pip3 install -r REQUIREMENTS.txt
---
+++ ZipMaster.py
@@ -0,0 +1,71 @@
+# MAKE SURE YOU HAVE THE LATEST PYTHON INSTALLED
+
+# AND RUN THIS COMMAND:
+#   LINUX: pip3 install zipfile
+#   WINDOWS: pip install zipfile
+
+'''imports'''
+import zipfile # make sure to install this module 
+import sys
+import os
+from datetime import datetime
+
+# This is ascii color codes
+class colors():
+    R = '\u001b[31m'
+    G = '\u001b[32m'
+    Y = '\u001b[33m'
+    M = '\u001b[35m'
+    RESET = '\u001b[0m'
+
+
+if os.name == 'nt': # if your on windows 
+    clear = 'cls'
+else: # if your on linux or mac
+    clear = 'clear'
+
+
+try:
+    os.system(clear)
+    print(colors.M + '''
+     ███████╗██╗██████╗ ███╗   ███╗ █████╗ ███████╗████████╗███████╗██████╗ 
+     ╚══███╔╝██║██╔══██╗████╗ ████║██╔══██╗██╔════╝╚══██╔══╝██╔════╝██╔══██╗
+       ███╔╝ ██║██████╔╝██╔████╔██║███████║███████╗   ██║   █████╗  ██████╔╝
+      ███╔╝  ██║██╔═══╝ ██║╚██╔╝██║██╔══██║╚════██║   ██║   ██╔══╝  ██╔══██╗
+     ███████╗██║██║     ██║ ╚═╝ ██║██║  ██║███████║   ██║   ███████╗██║  ██║
+     ╚══════╝╚═╝╚═╝     ╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═╝''' + colors.RESET)
+    print(colors.Y + "                             By: ToastedWaffless" + colors.RESET)
+    wordfile = input("WORDFILE: ")
+    lockedzip = input("LOCKED ZIP-FILE: ")
+    t1 = datetime.now() # gets the time when it starts to brute-force 
+    count = 1
+    num_words = len(list(open(wordfile, "rb"))) # gets the amount of words in your wordfile
+    with open(wordfile,'rb') as text:
+        for entry in text.readlines(): # reads all the lines in your wordfile
+            password = entry.strip()
+            try:
+                # tries to unzip the folder with the words in your wordfile
+                with zipfile.ZipFile(lockedzip,'r') as zf:
+                    zf.extractall(pwd=password) # extracts the folder
+                    data = zf.namelist()[0] # Whats in the folder
+                    data_size = zf.getinfo(data).file_size #how big the folder is
+                    t2 = datetime.now() # ending time
+                    total_time = t2 - t1 # gets the time it took
+                    print(f"{colors.G}\n\n****************************************\n ~ PASSWORD CRACKED! [WORD: {number}/{num_words}]\n ~ CRACKED PASSWORD: {password.decode('utf8')}\n ~ CONTENTS: {data}\n ~ SIZE: {data_size} bytes\n ~ TIME TOOK: {total_time}\n****************************************{colors.RESET}")
+                    break
+            # if the word is incorrect
+
+            except:
+
+                # if you want to not say what word was wrong comment or delete the line below                                             
+                print(colors.R + "[+] ~ INCORRECT PASSWORD : " + password.decode("utf8"))
+                number = count
+                count += 1
+                pass
+
+# if you hit ctrl + c it wont throw an error
+except KeyboardInterrupt:
+    print(colors.R + "\n\nGoodbye...")
+    sys.exit(0)
+
+
